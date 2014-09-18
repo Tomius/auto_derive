66e094b4b71c5f3902a53cd792e52dc4539a4b12
Tomius <tomius1994@gmail.com>
2014-09-18 17:13:15 +0200

    Foo

--- main.cpp
+++ main.cpp
@@ -9,4 +9,7 @@ int main() {
   constexpr auto func = 2+(x+x)+7+x+y+4+x;
   constexpr double derivate = func.gradient<VARIABLE(x)>();
   static_assert(derivate==4, "error");
+
+  constexpr double derivate2 = func.gradient<VARIABLE(y)>();
+  static_assert(derivate2==1, "error");
 }
